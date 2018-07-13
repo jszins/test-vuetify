36e6c4325751f62ac3ef728bae3b07b4a8f3fc5a
Jordan <jszins@gmail.com>
2018-07-13 15:03:22 -0500

    elasticsearch installed

--- src/components/Search/Search.vue
+++ src/components/Search/Search.vue
@@ -1,18 +1,46 @@
 <template>
     <div id="search">
-        <v-app dark>
+        <v-app light>
             <v-content>
-                <v-container>
-                    <v-layout
-                        column
-                        justify-center
-                        align-center>
-                        <v-flex xs12>
-                            <h1>Search Page</h1>
-                        </v-flex>
-                    </v-layout>
-                </v-container> 
+                <v-form>
+                    <v-container>
+                        <v-layout row wrap>
+                            <v-flex xs12 sm6>
+                            <v-text-field
+                                label="Search"
+                                single-line
+                                v-model="query"
+                            ></v-text-field>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </v-form>
             </v-content>
         </v-app>
     </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data () {
+        return {
+            query: '',
+            results: []
+        }
+    },
+    methods: {
+        search () {
+            axios.get("http://127.0.0.1:5601/search?q=" + this.query)
+                .then(res => console.log(res))
+                .catch(err => console.log(err))
+        }
+    },
+    watch: {
+        query () {
+            this.search();
+        }
+    }
+}
+</script>
