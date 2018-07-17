ba83ae943ff1c01bb1c94a68bd82d4eaa5869dfa
Jordan <jszins@gmail.com>
2018-07-17 16:30:15 -0500

    cleanup

--- src/components/Activities/Activity.vue
+++ src/components/Activities/Activity.vue
@@ -13,48 +13,6 @@
                         </v-layout>
                     </v-container>
                 </v-jumbotron>
-                <v-container
-                    fluid
-                    grid-list-{xs through xl}
-                >
-                    <v-layout
-                        row
-                        wrap    
-                    >
-                        <v-flex
-                            v-for="activity in activities" 
-                            :key="activity.id"
-                            lg3
-                            xs12
-                            class="pa-2"
-                        >
-                            <v-card color="blue-grey darken-3" >
-                                <v-card-media
-                                    :src="activity.img"
-                                    height="200px"
-                                ></v-card-media>
-
-                                <v-card-title primary-title>
-                                <div>
-                                    <h3 class="headline mb-0">{{ activity.name }}</h3>
-                                    <div> {{ activity.info }} </div>
-                                </div>
-                                </v-card-title>
-
-                                <v-card-actions>
-                                    <v-btn 
-                                        flat 
-                                        color="orange"
-                                        active-class="disabled"
-                                        :to="{ name: 'activityDetails', params: { id: activity.id} }"
-                                    >
-                                        Check this out
-                                    </v-btn>
-                                </v-card-actions>
-                            </v-card>
-                        </v-flex>
-                    </v-layout>
-                </v-container>
             </v-content>
         </v-app>
     </div>
