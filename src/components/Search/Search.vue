8f986ec542e614c4cebecb9eeaa77c698d90caeb
Jordan <jszins@gmail.com>
2018-07-17 12:37:16 -0500

    fully functional searching

--- src/components/Search/Search.vue
+++ src/components/Search/Search.vue
@@ -5,16 +5,64 @@
                 <v-form>
                     <v-container>
                         <v-layout row wrap>
-                            <v-flex xs12 sm6>
-                            <v-text-field
-                                label="Search"
-                                single-line
-                                v-model="query"
-                                @submit="search"
-                            ></v-text-field>
-                            <v-btn @click="search">Try</v-btn>
-                            <p>{{ results }}</p>
+                            <v-flex xs4 offset-xs4>
+                                <v-text-field
+                                    label="Search"
+                                    single-line
+                                    v-model="query"
+                                    @submit="search"
+                                ></v-text-field>
                             </v-flex>
+                            <v-flex xs12 sm6>
+                                <v-list two-line>
+                                    <v-subheader>{{ staffHead }}</v-subheader>
+                                    <v-btn
+                                        round
+                                        color="info"
+                                        @click="toggleStaff"
+                                    >
+                                        Toggle
+                                    </v-btn>
+                                    <template 
+                                        v-for="(result, i) in staffBuf"
+                                        v-if="staffEnabler"
+                                    >                                        
+                                        <v-list-tile
+                                            :key="i"
+                                        >
+                                        <v-list-tile-content>
+                                            <v-list-tile-title v-html="result._source.Name"></v-list-tile-title>
+                                            <v-list-tile-sub-title v-html="result._source.Position"></v-list-tile-sub-title>
+                                        </v-list-tile-content>
+                                        </v-list-tile>
+                                    </template>
+                                </v-list>
+                            </v-flex>      
+                            <v-flex xs12 sm6>
+                                <v-list two-line>
+                                    <v-subheader>{{ schoolHead }}</v-subheader>
+                                    <v-btn 
+                                        @click="toggleSchool" 
+                                        color="info"
+                                        round
+                                    >
+                                        Toggle
+                                    </v-btn>
+                                    <template 
+                                        v-for="(result, i) in schoolBuf"
+                                        v-if="schoolEnabler"
+                                    >                                        
+                                        <v-list-tile
+                                            :key="i"
+                                        >
+                                        <v-list-tile-content>
+                                            <v-list-tile-title v-html="result._source.Organization"></v-list-tile-title>
+                                            <v-list-tile-sub-title v-html="result._source.Physical_City"></v-list-tile-sub-title>
+                                        </v-list-tile-content>
+                                        </v-list-tile>
+                                    </template>
+                                </v-list>
+                            </v-flex>  
                         </v-layout>
                     </v-container>
                 </v-form>
@@ -30,13 +78,53 @@ export default {
     data () {
         return {
             query: '',
-            results: []
+            staff: [],
+            staffBuf: [],
+            schools: [],
+            schoolBuf: [],
+            staffEnabler: true,
+            staffHead: 'Staff',
+            schoolEnabler: true,
+            schoolHead: 'Schools'
         }
     },
     methods: {
         search () {
-            axios.get('http://localhost:9200/staff/_doc/_search?size=100&q=*:' + this.query)
-                .then(res => this.results.push(res.Name))
+            this.staffBuf = this.staff
+            this.schoolBuf = this.schools
+            if(this.query == ''){
+                this.staffBuf = []
+                this.schoolBuf = []
+                return
+            }  
+            this.staff = []
+            this.schools = []
+            axios.get('http://localhost:9200/staff/_doc/_search?size=25&q=*:' + this.query)
+                .then(res => this.staff = res.data.hits.hits)
+                .then(() => this.staffBuf = this.staff)
+                .catch(err => console.log(err))
+            axios.get('http://localhost:9200/schools/_doc/_search?size=50&q=*:' + this.query)
+            .then(res => this.schools = res.data.hits.hits)
+            .then(() => this.schoolsBuf = this.schools)
+            .catch(err => console.log(err))
+        },
+        toggleStaff() {
+            this.staffEnabler = !this.staffEnabler
+            if(this.staffEnabler == true){
+                this.staffHead = 'Staff'
+            }
+            else{
+                this.staffHead = 'Staff - Disabled'
+            }
+        },
+        toggleSchool() {
+            this.schoolEnabler = !this.schoolEnabler
+            if(this.schoolEnabler == true){
+                this.schoolHead = 'Schools'
+            }
+            else{
+                this.schoolHead = 'Schools - Disabled'
+            }
         }
     },
     watch: {
